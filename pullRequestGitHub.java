099916fe719f6fcca35a92e1ee3f30c41297123c
alumnociclo <alumnociclo@dptoinformatica.iesfuengirola1.net>
2023-10-05 18:03:41 +0200

    Añadiendo cambios a la rama creada

--- pullRequestGitHub.java
+++ pullRequestGitHub.java
@@ -7,5 +7,7 @@ public class pullRequestGitHub {
 
     public static void main(String args[]) {
         System.out.println("Tarea Git 5: Pull Request en GitHub y Tags de git (por parejas)");
+
+        System.out.println("Hola soy jorge y estoy modificando la copia del repositorio de Eris para luego subirlo a su repositorio :). Para ello he creado la RamaJorge");
     }
 }
